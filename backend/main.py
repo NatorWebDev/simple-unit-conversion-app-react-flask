dd5606d682bb59089a8d2799caab87fda7b48ed9
Luis Velasquez <126914912+NatorWebDev@users.noreply.github.com>
2025-08-15 18:38:18 -0400

    first

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route("/length")
+def length():
+    pass
+
+
+@app.route("/weight")
+def weight():
+    pass
+
+
+@app.route("/temperature")
+def temperature():
+    pass
+
+
+@app.route("/")
+def home():
+    return "<h1>Hola</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
